5daeafaea87e0916f0398bbae0374ef2f1cb6532
Julia Fernandez <julfervil@alum.us.es>
2026-05-08 23:57:13 +0200

    añadimos para desplegar en la nube railway

--- src/main/java/aiss/YouTubeMiner/service/VideoMinerService.java
+++ src/main/java/aiss/YouTubeMiner/service/VideoMinerService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpHeaders; // IMPORT CORRECTO
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
 @Service
@@ -15,20 +15,18 @@ public class VideoMinerService {
     @Autowired
     private RestTemplate restTemplate;
 
+    // Si lo abrimos desde Railway usará VIDEOMINER_URL, si no, localhost por defecto
     @Value("${videominer.base.url:http://localhost:8080}")
     private String videoMinerUrl;
 
     public Channel postChannel(Channel channel) {
-        String url = "http://localhost:8080/videominer/api/channels";
+        String url = videoMinerUrl + "/videominer/api/channels";
 
-        // 1. Creamos las cabeceras y metemos la API Key
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-API-KEY", "trabajoAiss123");
 
-        // 2. Metemos el canal y las cabeceras en un "paquete" llamado HttpEntity
         HttpEntity<Channel> request = new HttpEntity<>(channel, headers);
 
-        // 3. Hacemos el POST enviando el paquete completo (request)
         return restTemplate.postForObject(url, request, Channel.class);
     }
 }
